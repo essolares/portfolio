d41d937b7e6814ba0649da0f3bcbfe463ae645ac
Edgar Salazar <essolares@gmail.com>
2021-04-05 18:25:45 -0600

    Updates

---
+++ precache-manifest.4b35a008c17ec5ac3cc5f0b904c85dcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504187c38379aff5da089c4c69318564",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4529aec39e7fbb55bfa4",
+    "url": "/portfolio/static/css/2.49ecd109.chunk.css"
+  },
+  {
+    "revision": "a5c6ba96550d689e6e8d",
+    "url": "/portfolio/static/css/main.7fa8a2b8.chunk.css"
+  },
+  {
+    "revision": "4529aec39e7fbb55bfa4",
+    "url": "/portfolio/static/js/2.14c5678f.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/portfolio/static/js/2.14c5678f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c6ba96550d689e6e8d",
+    "url": "/portfolio/static/js/main.7e9b3ce6.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
